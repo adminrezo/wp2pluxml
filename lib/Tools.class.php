c7c1dc3cbc615f65421c0b769044db0d8a607e6e
Nicolas Lœuillet <nicolas@coteo.com>
2013-03-04 11:15:58 +0100

    first commit

---
+++ lib/Tools.class.php
@@ -0,0 +1,594 @@
+<?php
+/**
+ * Classe Tools
+ *
+ * Contient les outils nécessaires à la création des fichiers XML qui seront
+ * intégrés à PluXml
+ *
+ * @todo pour les méthodes removeNamespaceFromXML(),convertPathMedias(),
+ * convertSHPTagToHTML(), getXmlWordpressFiles() et displayListXmlWordpressFiles(),
+ * voir pour les déplacer dans Wordpress.class.php
+ * @todo optimiser le script quand on appelle getRubriqueIdByUrl() : ne plus
+ * parser le fichier complet mais parcourir un tableau qui doit être + rapide
+ * @todo mettre $config en globale
+ * @category   wp2pluxml
+ * @author     Nicolas Loeuillet <nicolas.loeuillet@gmail.com>
+ * @copyright  2010
+ * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
+ */
+
+error_reporting(0);
+
+class Tools
+{
+    /**
+     * Nettoie une chaine de caractères en remplaçant les balises
+     * [sourcecode language='...']...[/sourcecode] créées par le plugin
+     * Wordpress SyntaxHighlighterPlus
+     *
+     * @access public
+     * @param string $content chaine à nettoyer
+     * @return string chaine nettoyée
+     */
+    private function convertSHPTagToHTML($content)
+    {
+        $regex       = "#\[sourcecode language=(.+?)\](.+?)\[\/sourcecode\]#s";
+        $replacement = "<code class=$1>$2</code>";
+        $content     = preg_replace($regex, $replacement, $content);
+
+        return $content;
+    }
+
+    /**
+     * Nettoie le XML généré par le plugin d'export de Wordpress
+     *
+     * @access public
+     * @param string $source export XML d'un blog Wordpress
+     * @return string document XML nettoyé
+     */
+    private function cleanXML($source)
+    {
+        $namespaces = array (
+            'wp:category'             => 'category',
+            'wp:category_nicename'    => 'category_nicename',
+            'wp:cat_name'             => 'cat_name',
+            'wp:post_name'            => 'post_name',
+            'wp:post_date'            => 'post_date',
+            'wp:comment_status'       => 'comment_status',
+            'wp:comment'              => 'comment',
+            'excerpt:encoded'         => 'excerpt',
+            'content:encoded'         => 'content',
+            'wp:comment_author'       => 'comment_author',
+            'wp:comment_author_email' => 'comment_author_email',
+            'wp:comment_author_url'   => 'comment_author_url',
+            'wp:comment_author_IP'    => 'comment_author_IP',
+            'wp:comment_date'         => 'comment_date',
+            'wp:comment_content'      => 'comment_content',
+            'wp:comment_approved'     => 'comment_approved',
+        );
+
+        foreach ($namespaces as $namespace => $replace) {
+            $source = str_replace("<" . $namespace . ">",
+                    "<" . $replace . ">", $source);
+            $source = str_replace("</" . $namespace . ">",
+                    "</" . $replace . ">", $source);
+        }
+
+        $source = self::convertSHPTagToHTML($source);
+        $source = self::convertPathMedias($source);
+        
+        return $source;
+    }
+
+    /**
+     * Permet de changer le chemin des médias du blog Wordpress avec
+     * le bon chemin dans PluXml
+     *
+     * @access private
+     * @todo ne pas laisser les chemins en dur
+     * @param string $source export XML d'un blog Wordpress
+     * @return string document XML avec les chemins des médias modifiés
+     */
+    private function convertPathMedias($source)
+    {
+        $source = str_replace("wp-content/uploads", "data/images", $source);
+
+        return $source;
+    }
+
+    /**
+     * Crée un fichier XML PluXml en fonction du billet
+     *
+     * @access private
+     * @param Wordpress $billet correspond à un billet
+     * @return SimpleXMLExtend fichier xml correspondant au billet
+     */
+    private function generateXmlForExport(Wordpress $billet)
+    {
+        $output = "<?xml version='1.0' encoding='UTF-8'?>" . "\n" .
+                    "<document></document>";
+
+        $xml = new SimpleXMLExtend($output);
+
+        $infopost = $xml->addChild("infopost");
+        $infopost->addCData("title", $billet->getTitle());
+        $infopost->addCData("author", $billet->getAuthor());
+        $infopost->addChild("allow_com", $billet->getAllowCom());
+
+        $xml->addCData("chapo", $billet->getChapo());
+        $xml->addCData("content", $billet->getContent());
+
+        return $xml->asXML();
+    }
+
+    /**
+     * Crée un fichier XML PluXml pour un commentaire
+     *
+     * @access private
+     * @param array $comment correspond à un commentaire
+     * @return SimpleXMLExtend fichier xml correspondant au commentaire
+     */
+    private function generateXmlCommentsForExport($comment)
+    {
+        $output = "<?xml version='1.0' encoding='UTF-8'?>" . "\n" .
+                    "<comment></comment>";
+
+        $xml = new SimpleXMLExtend($output);
+
+        $xml->addCData("author", $comment['comment_author']);
+        $xml->addChild("type", 'normal');
+        $xml->addChild("ip", $comment['comment_author_IP']);
+        $xml->addCData("mail", $comment['comment_author_email']);
+        $xml->addCData("site", $comment['comment_author_url']);
+        $xml->addCData("content", $comment['comment_content']);
+
+        return $xml->asXML();
+    }
+
+    /**
+     * Écrit du contenu XML dans un fichier
+     *
+     * @param string $xml contenu XML à écrire
+     * @param string $output chemin du fichier
+     * @param string $mode mode d'ouverture du fichier
+     * @return boolean
+     */
+    private function writeXMLinFile($xml, $output, $mode = 'w')
+    {
+        if (!$handle = fopen($output, $mode)) {
+            Tools::log('Erreur lors de l\'ouverture du fichier '. $output, __LINE__);
+            return FALSE;
+        }
+
+        if (!fwrite($handle, $xml)) {
+            Tools::log('Erreur lors de l\'écriture dans le fichier '. $output, __LINE__);
+            return FALSE;
+        }
+
+        fclose($handle);
+
+        return TRUE;
+    }
+
+    /**
+     * Écrit dans le fichier XML le contenu du billet
+     *
+     * @access private
+     * @param Wordpress $billet correspond à un billet
+     * @param array $config configuration de wp2pluxml
+     * @return Boolean TRUE si tout se passe bien, FALSE sinon
+     */
+    private function generateXmlFile(Wordpress $billet, $config)
+    {
+        $output = $config['racine_articles'] . $billet->getFilename();
+        $xml    = self::generateXmlForExport($billet);
+
+        if (!self::writeXMLinFile($xml, $output)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Écrit dans le fichier XML le contenu du commentaire
+     *
+     * @access private
+     * @param Wordpress $billet correspond à un billet
+     * @param array $config configuration de wp2pluxml
+     * @return Boolean TRUE si tout se passe bien, FALSE sinon
+     */
+    private function generateXmlCommentsFile(Wordpress $billet, $config)
+    {
+        foreach ($billet->getComments() as $comment) {
+            $output = $config['racine_commentaires'] .
+                $billet->getId() . '.' . self::getCommentDateForFilename($comment) . '-1.xml';
+            $xml    = self::generateXmlCommentsForExport($comment);
+
+            if (!self::writeXMLinFile($xml, $output)) {
+                return FALSE;
+            }
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Formate la date du commentaire pour être utilisée dans le nom du fichier
+     * au format timestamp
+     *
+     * @access private
+     * @return string
+     */
+    private function getCommentDateForFilename($comment)
+    {
+        $timestamp = mktime(substr($comment['comment_date'], 11, 2),
+                substr($comment['comment_date'], 14, 2), 0,
+                substr($comment['comment_date'], 5, 2),
+                substr($comment['comment_date'], 8, 2),
+                substr($comment['comment_date'], 0, 4)
+                );
+        
+        return $timestamp;
+    }
+
+    /**
+     * Teste si un fichier a une extension autorisée
+     *
+     * @access private
+     * @param string $file nom complet du fichier
+     * @return boolean TRUE si l'extension du fichier est autorisée, FALSE sinon
+     */
+    private function isAvailableExtension($file)
+    {
+        $available_extensions = array('xml');
+        
+        return (in_array(substr(strrchr($file,'.'), 1), $available_extensions) ? TRUE : FALSE);
+    }
+
+    /**
+     * Génère un tableau contenant le nom des fichiers XML dans le répertoire
+     * principal de wp2pluxml
+     *
+     * @access private
+     * @return Array tableau avec les noms des fichiers XML
+     */
+    private function getXmlWordpressFiles()
+    {
+        $files = array();
+        
+        if (!$handle = opendir(".")) {
+            Tools::log('Erreur lors du parcours du répertoire principal.', __LINE__);
+            return FALSE;
+        }
+        else {
+            while (FALSE !== ($filename = readdir($handle))) {
+                if (Tools::isAvailableExtension($filename)) {
+                    $files[] = $filename;
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    /**
+     * Retourne la liste des fichiers XML présents dans le répertoire de
+     * wp2pluxml
+     *
+     * @access public
+     * @return array
+     */
+    public function displayListXmlWordpressFiles()
+    {
+        $liste = array();
+        $files = Tools::getXmlWordpressFiles();
+        foreach ($files as $file) {
+            $liste[$file] = $file;
+        }
+
+        return $liste;
+    }
+
+    /**
+     * Vérifie si le nom du fichier est correct :
+     * - le fichier existe
+     * - l'extension est autorisée
+     * - le fichier n'est pas le nom du répertoire courant ou précédent
+     *
+     * @access private
+     * @param string $filename nom du fichier à vérifier
+     * @return boolean TRUE si le nom du fichier est correct, FALSE sinon
+     */
+    private function checkFilename($filename)
+    {
+        if (!Tools::isAvailableExtension($filename)) {
+            Tools::log('Extension pas disponible pour le fichier ' . $filename, __LINE__);
+            return FALSE;
+        }
+
+        if (!file_exists($filename)) {
+            Tools::log('Le fichier ' . $filename . 'n\'existe pas.', __LINE__);
+            return FALSE;
+        }
+
+        if ('.' == $filename || '..' == $filename) {
+            Tools::log('Le nom du fichier n\'est pas correct.', __LINE__);
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Crée un fichier XML pour les catégories
+     *
+     * @access private
+     * @param array $items correspond aux catégories
+     * @param array $config configuration de wp2pluxml
+     * @return SimpleXMLExtend fichier xml correspondant au commentaire
+     */
+    private function generateXmlCategoriesForExport($items, $config)
+    {
+        $output = "<?xml version='1.0' encoding='UTF-8'?>" . "\n" .
+                    "<document></document>";
+
+        $xml = new SimpleXMLExtend($output);
+
+        $count = 1;
+        foreach ($items as $item) {
+            $number = sprintf("%03s", $count);
+            $attr   = array(
+                'number' => $number,
+                'tri'    => $config['tri'],
+                'bypage' => $config['bypage'],
+                'menu'   => $config['menu'],
+                'url'    => $item->category_nicename,
+
+            );
+            $node = $xml->addCData('categorie', $item->cat_name, $attr);
+
+            if (!Tools::addCategorieToHtaccess($item, $number, $config)) {
+                return FALSE;
+            }
+
+            $count ++;
+        }
+
+        return $xml->asXML();
+    }
+
+    /**
+     * Écrit dans le fichier XML les catégories
+     *
+     * @access private
+     * @param array $items correspond aux catégories
+     * @param array $config configuration de wp2pluxml
+     * @return Boolean TRUE si tout se passe bien, FALSE sinon
+     */
+    private function generateXmlCategoriesFile($items, $config)
+    {
+        $xml = self::generateXmlCategoriesForExport($items, $config);
+
+        if (!self::writeXMLinFile($xml, $config['categories'])) {
+            return FALSE;
+        }
+
+        return $xml;
+    }
+
+    /**
+     * Retrouve l'identifiant d'une rubrique en fonction de son URL
+     *
+     * @access private
+     * @param string $categories XML des catégories
+     * @param string $rubrique_url URL d'une rubrique
+     * @return string
+     */
+    private function getRubriqueIdByUrl($categories, $rubrique_url)
+    {
+        $xml      = @simplexml_load_string($categories);
+        $elements = $xml->categorie;
+
+        foreach ($elements as $element) {
+            if ((string) $element['url'] == $rubrique_url)
+            {
+                return $element['number'];
+            }
+        }
+
+        return '001';
+    }
+
+    /**
+     * Vérifie si le fichier XML de l'export Wordpress est correct
+     *
+     * @param string $file URL du fichier XML à parser
+     * @return boolean
+     */
+    private function checkWordpressExport($file)
+    {
+        if ('' == $file) {
+            Tools::log('Le fichier XML de l\'export Wordpress n\'est pas correct', __LINE__);
+            return FALSE;
+        }
+
+        if (!self::checkFilename($file)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Convertit le fichier XML de Wordpress en un objet XML
+     *
+     * @param string $file URL du fichier XML à parser
+     * @return SimpleXMLElement
+     */
+    private function getXmlFromWordpressExport($file)
+    {
+        $data = file_get_contents($file);
+
+        if (!$data) {
+            Tools::log('Erreur lors de la récupération du contenu du fichier XML', __LINE__);
+            return NULL;
+        }
+
+        $data = self::cleanXML($data);
+
+        if (!$data) {
+            Tools::log('Erreur lors du nettoyage du fichier XML', __LINE__);
+            return NULL;
+        }
+
+        return @simplexml_load_string($data);
+    }
+
+    /**
+     * Lance la conversion des catégories
+     *
+     * @param SimpleXMLElement $categories noeud XML correspondant aux catégories
+     * @param array $config configuration de wp2pluxml
+     * @return object
+     */
+    public function convertCategories($categories, $config)
+    {
+        $items = $categories;
+
+        if (count($items) == 0) {
+            Tools::log('Le tableau $items des catégories est vide', __LINE__);
+            return NULL;
+        }
+
+        if (!($categories = self::generateXmlCategoriesFile($items, $config))) {
+            return NULL;
+        }
+
+        return $categories;
+    }
+
+    /**
+     * Génère la ligne à ajouter dans le .htaccess pour les catégories
+     *
+     * @param SimpleXMLElement $categorie noeud XML correspondant à la catégorie
+     * @param integer $number identifiant de la catégorie sous PluXml
+     * @param array $config configuration de wp2pluxml
+     * @return boolean
+     */
+    private function addCategorieToHtaccess($categorie, $number, $config)
+    {
+        $content = 'RedirectPermanent /category/'. $categorie->category_nicename . ' /?categorie' .
+            $number . '/' . $categorie->category_nicename . "\r\n";
+
+        if (!Tools::writeXMLinFile($content, '../.htaccess', 'a')) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Génère la ligne à ajouter dans le .htaccess pour les billets
+     *
+     * @param Wordpress $billet correspond à un billet
+     * @param array $config configuration de wp2pluxml
+     * @return boolean
+     */
+    private function addBilletToHtaccess(Wordpress $billet, $config)
+    {
+        $url_originale = str_replace($config['url_blog'], '', $billet->getUrl());
+        $content       = 'RedirectPermanent '. $url_originale . ' /?article' .
+            $billet->getId() . '/' . $billet->getUrlFile() . "\r\n";
+
+        if (!Tools::writeXMLinFile($content, '../.htaccess', 'a')) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Lance la conversion des billets et des commentaires
+     *
+     * @param SimpleXMLElement $items noeud XML correspondant aux billets
+     * @return boolean
+     */
+    public function convertPostsAndComments($items, $categories, $config)
+    {
+        $id = 1;
+
+        if (count($items) == 0) {
+            Tools::log('Le tableau $items pour les billets est vide', __LINE__);
+            return FALSE;
+        }
+
+        foreach ($items as $item) {
+            $billet = new Wordpress($item);
+            $billet->setId($id);
+            $billet->setAuthor($config['author']);
+
+            $rubrique_id = self::getRubriqueIdByUrl($categories, $billet->getRubriqueUrl());
+            $billet->setRubriqueId($rubrique_id);
+
+            if (!self::generateXmlFile($billet, $config)) {
+                return FALSE;
+            }
+
+            if (!self::generateXmlCommentsFile($billet, $config)) {
+                return FALSE;
+            }
+
+            if (!Tools::addBilletToHtaccess($billet, $config)) {
+                return FALSE;
+            }
+
+            $id ++;
+        }
+
+        return TRUE;
+    }
+
+    /**
+     * Permet de logger un texte dans le ficheir de log de wp2pluxml
+     *
+     * @param string $text texte à logger
+     * @param integer $line numéro de la ligne
+     */
+
+    public function log($text, $line)
+    {
+        $log =& new oolog("wp2pluxml.log", FILE | DEBUG);
+        $log->log($text, DEBUG, false, $line);
+        $log->closelog();
+    }
+
+    /**
+     * Lancement de la conversion vers PluXml
+     *
+     * @param mixed $post Données $_POST passées sur le script PluXml
+     * @param array $config Tableau de configuration pour wp2pluxml
+     */
+    public function convert2pluxml($post, $config)
+    {
+        $filename_wp = (isset($post['file']) ? $post['file'] : '');
+
+        if (!self::checkWordpressExport($filename_wp)) {
+            return FALSE;
+        }
+
+        if (!($xml = Tools::getXmlFromWordpressExport($filename_wp))) {
+            return FALSE;
+        }
+
+        if (!($categories = self::convertCategories($xml->channel->category, $config))) {
+            return FALSE;
+        }
+
+        if (!self::convertPostsAndComments($xml->channel->item, $categories, $config)) {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+}
---
+++ lib/Wordpress.class.php
@@ -0,0 +1,409 @@
+<?php
+/**
+ * Classe Wordpress
+ *
+ * Correspond à un billet PluXml avec les données de Wordpress
+ *
+ * @category   wp2pluxml
+ * @author     Nicolas Loeuillet <nicolas.loeuillet@gmail.com>
+ * @copyright  2010
+ * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
+ */
+
+class Wordpress
+{
+    /**
+     * Identifiant du billet sur 4 caractères (0000)
+     * @var string
+     */
+    private $id;
+    /**
+     * Titre du billet
+     * @var string
+     */
+    private $title;
+    /**
+     * Permet de savoir si on peut poster des commentaires. Si $allow_com vaut
+     * 1, on peut poster. S'il vaut 0, on ne peut pas.
+     * @var string
+     */
+    private $allow_com;
+    /**
+     * Chapeau du billet au format HTML
+     * @var string
+     */
+    private $chapo;
+    /**
+     * Contenu du billet au format HTML
+     * @var string
+     */
+    private $content;
+    /**
+     * URL du billet pour le nom du fichier
+     * @var string
+     */
+    private $url_file;
+    /**
+     * URL du billet
+     * @var string
+     */
+    private $url;
+    /**
+     * Date de publication du billet
+     * @var string
+     */
+    private $date;
+    /**
+     * Nom de l'auteur des billets
+     * @var string
+     */
+    private $author;
+    /**
+     * URL de la rubrique
+     * @var string
+     */
+    private $rubrique_url;
+    /**
+     * Identifiant de la rubrique qui contient le billet sur 3 caractères (000)
+     * @var string
+     */
+    private $rubrique_id;
+    /**
+     * Liste des commentaires du billet
+     * @var array
+     */
+    private $comments;
+
+    /**
+     * Constructeur
+     *
+     * @access public
+     * @param SimpleXMLElement $item
+     */
+    public function  __construct(SimpleXMLElement $item)
+    {
+        $this->setTitle($item->title);
+        $this->setAllowCom($item->comment_status);
+        $this->setChapo($item->excerpt);
+        $this->setContent($item->content);
+        $this->setUrl($item->link);
+        $this->setUrlFile($item->post_name);
+        $this->setDate($item->post_date);
+        $this->setRubriqueUrl($item->category);
+        $this->setComments($item->comment);
+    }
+
+    /**
+     * Setter de l'identifiant
+     *
+     * @access public
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $id = sprintf("%04s", $id);
+        $this->id = $id;
+    }
+
+    /**
+     * Getter de l'identifiant
+     *
+     * @access public
+     * @return string identifiant sur 4 caractères (0000)
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Setter de l'identifiant
+     *
+     * @access public
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Getter du titre
+     *
+     * @access public
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Setter de allow_com (commentaires autorisés)
+     *
+     * @access public
+     * @param string $comment_status
+     */
+    public function setAllowCom($comment_status)
+    {
+        $this->allow_com = ($comment_status == 'open') ? '1' : '0';
+    }
+
+    /**
+     * Getter de allow_com
+     *
+     * @access public
+     * @return integer
+     */
+    public function getAllowCom()
+    {
+        return $this->allow_com;
+    }
+
+    /**
+     * Setter du chapeau
+     *
+     * @access public
+     * @param string $chapo
+     */
+    public function setChapo($chapo)
+    {
+        $this->chapo = nl2br($chapo);
+    }
+
+    /**
+     * Getter du chapeau
+     *
+     * @access public
+     * @return string
+     */
+    public function getChapo()
+    {
+        return $this->chapo;
+    }
+
+    /**
+     * Setter du contenu
+     *
+     * @access public
+     * @param string $content
+     */
+    public function setContent($content)
+    {        
+        $this->content = nl2br($content);
+    }
+
+    /**
+     * Getter du contenu
+     *
+     * @access public
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Setter de l'URL pour le nom du fichier
+     *
+     * @access public
+     * @param string $post_name
+     */
+    public function setUrlFile($post_name)
+    {
+        $toReplace = array('%c2%ab', '%c2%bb', '%c3%b8', '%d0%b0', '%e2%80%a6',
+            '%c2%a0', '%e2%80%a6', '%c2%a0a', '_', '/');
+        $post_name = str_replace($toReplace, '', $post_name);
+        $this->url_file = $post_name;
+    }
+
+    /**
+     * Getter de l'URL pour le nom du fichier
+     *
+     * @access public
+     * @return string
+     */
+    public function getUrlFile()
+    {
+        return $this->url_file;
+    }
+
+    /**
+     * Setter de l'URL
+     *
+     * @access public
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Getter de l'URL
+     *
+     * @access public
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Setter de la date
+     *
+     * @access public
+     * @param string $post_date
+     */
+    public function setDate($post_date)
+    {
+        $this->date = $post_date;
+    }
+
+    /**
+     * Getter de la date
+     *
+     * @access public
+     * @return string
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Setter de l'auteur
+     *
+     * @access public
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * Getter de l'auteur
+     *
+     * @access public
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Setter de l'URL de la première rubrique
+     *
+     * @access public
+     * @param array $categories ensemble des catégories du billet
+     */
+    public function setRubriqueUrl($categories)
+    {
+        $rubrique = '';
+        foreach ($categories as $category) {
+            if ($category['domain'] == 'category') {
+                $rubrique = $category['nicename'];
+                break;
+            }
+        }
+        $this->rubrique_url = $rubrique;
+    }
+
+    /**
+     * Getter de l'identifiant
+     *
+     * @access public
+     * @return string
+     */
+    public function getRubriqueUrl()
+    {
+        return $this->rubrique_url;
+    }
+
+    /**
+     * Setter de l'identifiant de la rubrique sur 3 caractères (000)
+     *
+     * @access public
+     * @param string $rubrique_id
+     */
+    public function setRubriqueId($rubrique_id)
+    {
+        $this->rubrique_id = $rubrique_id;
+    }
+
+    /**
+     * Getter de l'identifiant
+     *
+     * @access public
+     * @return string
+     */
+    public function getRubriqueId()
+    {
+        return $this->rubrique_id;
+    }
+	
+    /**
+     * Setter des commentaires
+     *
+     * @access public
+     * @param array $comments
+     */
+    public function setComments($comments)
+    {
+        $tabComments = array();
+        $count = 0;
+        foreach ($comments as $comment) {
+            if ($comment->comment_approved != 'spam') {
+                $tabComments[$count]['comment_author']       = $comment->comment_author;
+                $tabComments[$count]['comment_author_email'] = $comment->comment_author_email;
+                $tabComments[$count]['comment_author_url']   = $comment->comment_author_url;
+                $tabComments[$count]['comment_author_IP']    = $comment->comment_author_IP;
+                $tabComments[$count]['comment_date']         = $comment->comment_date;
+                $tabComments[$count]['comment_content']      = $comment->comment_content;
+                $count ++;
+            }
+        }
+
+        $this->comments = $tabComments;
+    }
+
+    /**
+     * Getter des commentaires
+     *
+     * @access public
+     * @return array
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Formate la date pour être utilisée dans le nom du fichier
+     *
+     * @access private
+     * @return string
+     */
+    private function getDateForFilename()
+    {
+        return substr($this->date, 0, 4) . substr($this->date, 5, 2) .
+            substr($this->date, 8, 2) . substr($this->date, 11, 2) .
+            substr($this->date, 14, 2);
+    }
+
+    /**
+     * Crée le nom du fichier
+     *
+     * @access public
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->id . '.' . $this->getRubriqueId() . '.' .
+            $this->getDateForFilename() . '.' . $this->url_file . '.xml';
+    }
+}
