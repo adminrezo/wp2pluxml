52d82bd445ad6628b7a9853ea660bab55530ea5e
Nicolas Lœuillet <nicolas@coteo.com>
2013-03-07 15:35:23 +0100

    Mise en page du fichier index.php

--- index.php
+++ index.php
@@ -15,10 +15,13 @@ define('PLX_CORE', PLX_ROOT.'core/');
 include(PLX_ROOT.'config.php');
 include(PLX_CORE.'lib/config.php');
 
-# On verifie que PluXml est installé
+$demarrer_installation = TRUE;
+
+# On vérifie que PluXml est installé
+$pluxml_actif = TRUE;
 if(!file_exists(path('XMLFILE_PARAMETERS'))) {
-    header('Location: '.PLX_ROOT.'install.php');
-    exit;
+    $pluxml_actif = FALSE;
+    $demarrer_installation = FALSE;
 }
 
 # On démarre la session
@@ -37,10 +40,10 @@ include(PLX_CORE.'lib/class.plx.show.php');
 include(PLX_CORE.'lib/class.plx.encrypt.php');
 include(PLX_CORE.'lib/class.plx.plugins.php');
 
-# Echappement des caractères
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $_POST = plxUtils::unSlash($_POST);
-}
+include_once 'lib/SimpleXMLExtend.class.php';
+include_once 'lib/Wordpress.class.php';
+include_once 'lib/Tools.class.php';
+include_once 'lib/oolog.class.php';
 
 # Configuration de base
 $config = array(
@@ -53,12 +56,41 @@ $config = array(
     'categories'          => PLX_ROOT.'data/configuration/categories.xml',
 );
 
-include_once 'lib/SimpleXMLExtend.class.php';
-include_once 'lib/Wordpress.class.php';
-include_once 'lib/Tools.class.php';
-include_once 'lib/oolog.class.php';
+# On vérifie que SimpleXML est installée
+$simplexml_actif = TRUE;
+if (!extension_loaded('simplexml')) {
+    $simplexml_actif = FALSE;
+    $demarrer_installation = FALSE;
+}
+
+# On vérifie qu'il y a bien un ou plusieurs fichiers d'export dans le répertoire de wp2pluxml
+$xml_presents = TRUE;
+if (($liste_fichiers = Tools::displayListXmlWordpressFiles()) == array()) {
+    $xml_presents = FALSE;
+    $demarrer_installation = FALSE;
+}
+
+# On vérifie que l'URL du blog est bien définie
+$url_definie = TRUE;
+if (!$config['url_blog']) {
+    $url_definie = FALSE;
+    $demarrer_installation = FALSE;
+}
+
+# On vérifie que le nom de l'auteur est bien défini
+$auteur_defini = TRUE;
+if (!$config['author']) {
+    $auteur_defini = FALSE;
+    $demarrer_installation = FALSE;
+}
+
+# Echappement des caractères
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $_POST = plxUtils::unSlash($_POST);
+}
 
 if(!empty($_POST['file'])) {
+    # Conversion !
     if (!Tools::convert2pluxml($_POST, $config)) {
         die('Une erreur a été rencontrée durant la conversion. Consultez les logs.');
     }
@@ -72,62 +104,71 @@ if(!empty($_POST['file'])) {
 <head>
 <title>wp2pluxml - Configuration</title>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo strtolower(PLX_CHARSET) ?>" />
-<link rel="stylesheet" href="knacss.css" media="all">
+<link rel="stylesheet" href="assets/knacss.css" media="all">
+<link rel="stylesheet" href="assets/style.css" media="all">
 </head>
-<body class="w90 mtl ml3">
+<body class="w960p mtl ml3">
 
-    <header>
-        <h1>wp2pluxml</h1>
+    <header class="col w150p mr2">
+        <h1><img src="assets/logo_wp2pluxml.jpg" alt="wp2pluxml" /></h1>
     </header>
 
-    <h2>Présentation</h2>
-    <div>
-        <a href="https://github.com/nicosomb/wp2pluxml">wp2pluxml</a> vous permet de convertir le contenu de <strong>votre blog Wordpress en un blog PluXml</strong>.
-    </div>
-    <div><b>Attention</b>, ce script est à exécuter en local et non en environnement de production
-        (selon la taille de votre blog Wordpress, la génération peut prendre du temps).
+    <div id="wrap" class="col">
+        <h2>wp2pluxml</h2>
+
+        <div>
+            <b>wp2pluxml</b> vous permet de convertir le contenu de votre blog Wordpress en un blog PluXml. Très simplement, les billets, les pages, les commentaires et les auteurs pourront être récupérés sur votre tout nouveau PluXml fraichement installé.
+        </div>
+
+        <h2>Pré-requis</h2>
+
+        <div>
+            <ul id="prerequis" class="w400p">
+                <li class="<?php echo ($url_definie ? 'actif' : 'inactif'); ?>">Modifier $config['url_blog'] (ligne 50 de ce fichier)</li>
+                <li class="<?php echo ($auteur_defini ? 'actif' : 'inactif'); ?>">Modifier $config['author'] (ligne 52 de ce fichier)</li>
+                <li class="<?php echo ($pluxml_actif ? 'actif' : 'inactif'); ?>">PluXml installé dans le répertoire parent</li>
+                <li class="<?php echo ($simplexml_actif ? 'actif' : 'inactif'); ?>">Extension SimpleXML installée</li>
+                <li class="<?php echo ($xml_presents ? 'actif' : 'inactif'); ?>">Fichier(s) d'export présent(s)</li>
+
+            </ul>
+        </div>
+
+        <h2>Go !</h2>
+
+        <?php if(!$demarrer_installation) : ?>
+        <div class="erreur">
+            Toutes les conditions ne sont pas réunies pour démarrer la conversion. Merci de relire les pré-requis ci-dessus.
+        </div>
+        <?php else: ?>
+
+        <div>
+            Sélectionnez votre export XML dans la liste déroulante et validez pour lancer la conversion. Allez boire un café. Revenez.
+        </div>
+        <div>
+            <form action="index.php" method="post">
+            <?php plxUtils::printSelect('file', $liste_fichiers) ?>
+                <input type="submit" value="Lancer la conversion" />
+            </form>
+        </div>
+
+        <div>
+            Vous êtes l'heureux possesseur d'un blog PluXml :) ! Enjoy.
+        </div>
+
+        <?php endif; ?>
+
+        <h2>Quelques liens</h2>
+        <ul>
+            <li><a href="https://github.com/nicosomb/wp2pluxml/issues/new">Rapporter un bug</a></li>
+            <li><a href="https://github.com/nicosomb/wp2pluxml/wiki">Documentation</a></li>
+            <li><a href="http://www.cdetc.fr/wp2pluxml/">Le blog du projet</a></li>
+        </ul>
+
     </div>
 
-    <h2>Pré-requis</h2>
-    <div>
-        Avant toute chose, sachez que ce script a besoin de PHP 5 pour fonctionner (en effet, nous utilisons <a href="http://fr.php.net/manual/fr/book.simplexml.php">l'extension SimpleXML</a>).<br />
-        Ce script est compatible à partir de PluXml 5.1.x et WordPress 3.5.x. Nous n'avons pas testé avec des versions antérieures, impossible donc de dire s'il est compatible avec ces versions. Il y a de fortes chances que ça ne fonctionne pas, PluXml ayant modifié le format de génération des fichiers XML.
-    </div>
-
-    <div>
-        Avant de faire quoique ce soit, ouvrez le fichier index.php de wp2pluxml avec votre éditeur préféré pour modifier le tableau de configuration, à partir de la ligne 45 (<code># Configuration de base</code>). Copiez le dossier wp2pluxml dans le répertoire de votre PluXml fraichement installé.
-    </div>
-
-    <h2>Export de vos données Wordpress</h2>
-    <div>
-        Pour générer un export XML depuis votre blog Wordpress, il faut installer et activer
-        le plugin <a href="http://wordpress.org/extend/plugins/advanced-export-for-wp-wpmu/">Advanced Export for WP & WPMU</a>
-        (dernière version testée, la 2.9). L'utilisation de ce plugin n'étant pas très complexe, nous nous passerons d'une documentation détaillée. Nous vous conseillons tout de même de n'exporter que les billets publiés (les brouillons et les pages seront traités par wp2pluxml prochainement).
-    </div>
-    <div>
-        Copiez l'export XML dans le même répertoire que celui de wp2pluxml. Actualisez la page pour afficher votre export dans la liste déroulante ci-dessous.
-    </div>
-    <div>
-        Concernant vos médias (images et documents) de Wordpress, copiez-les dans le répertoire data/images de votre blog PluXml. wp2pluxml se charge de modifier les chemins dans vos billets.
-    </div>
-
-    <h2>Go !</h2>
-    <div>
-        Sélectionnez votre export XML dans la liste déroulante et validez pour lancer la conversion. Allez boire un café. Revenez.
-    </div>
-    <div>
-        <form action="index.php" method="post">
-        <?php plxUtils::printSelect('file', Tools::displayListXmlWordpressFiles(), $selected, $readonly, $class) ?>
-        <input type="submit" value="Lancer la conversion" />
-        </form>
-    </div>
-    <div>
-        Vous êtes l'heureux possesseur d'un blog PluXml :) ! Enjoy.
-    </div>
-
-    <div class="right">
+    <footer class="right small">
         <a href="https://github.com/nicosomb/wp2pluxml/">wp2pluxml</a>, par <a href="http://www.cdetc.fr">Nicolas Lœuillet</a>
-    </div>
+    </footer>
 
 </body>
 </html>
